97af1bbecfdbc698d328dbdc7c98715fb61ec7b7
Amanda <amandaoliveirapolari@gmail.com>
2023-06-13 14:58:30 -0300

    Resolução do bug e correção de identação

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import { useState } from "react";
-import styled, { createGlobalStyle } from "styled-components";
-import pokemons from "./pokemon/pokemon.json";
-import PokemonCard from "./components/PokemonCard/PokemonCard";
-import { getColors } from "./utils/ReturnCardColor";
-import Header from "./components/Header/Header.js";
+import { useState } from 'react';
+import styled, { createGlobalStyle } from 'styled-components';
+import pokemons from './pokemon/pokemon.json';
+import PokemonCard from './components/PokemonCard/PokemonCard';
+import { getColors } from './utils/ReturnCardColor';
+import Header from './components/Header/Header.js';
 const GlobalStyle = createGlobalStyle`
   *{
     padding: 0;
@@ -14,42 +14,47 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 const CardsContainer = styled.div`
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(440px, 1fr));
-  justify-items: center;
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(440px, 1fr));
+    justify-items: center;
 `;
 function App() {
-  const [pesquisa, setPesquisa] = useState("");
-  const [idFilter, setIdFilter] = useState("");
+    const [pesquisa, setPesquisa] = useState('');
+    const [idFilter, setIdFilter] = useState('');
 
-  return (
-    <>
-      <GlobalStyle />
-      <Header
-        idFilter={idFilter}
-        setIdFilter={setIdFilter}
-        pesquisa={pesquisa}
-        setPesquisa={setPesquisa}
-      />
-      <CardsContainer>
-        {pokemons.filter((pokemon) => {
-          return idFilter ? pokemon.id.includes(idFilter) : pokemon
-        })
-          .filter((pokemon) => {
-            return pokemon.name.english.toLowerCase().includes(pesquisa.toLowerCase());
-          })
-          .map((pokemon) => {
-            return (
-              <PokemonCard
-                cardColor={getColors(pokemon.type[0])}
-                key={pokemon.id}
-                pokemon={pokemon}
-              />
-            );
-          })}
-      </CardsContainer>
-    </>
-  );
+    return (
+        <>
+            <GlobalStyle />
+            <Header
+                idFilter={idFilter}
+                setIdFilter={setIdFilter}
+                pesquisa={pesquisa}
+                setPesquisa={setPesquisa}
+            />
+            <CardsContainer>
+                {pokemons
+                    .filter((pokemon) => {
+                        return idFilter
+                            ? pokemon.id.includes(idFilter)
+                            : pokemon;
+                    })
+                    .filter((pokemon) => {
+                        return pokemon.name.english
+                            .toLowerCase()
+                            .includes(pesquisa.toLowerCase());
+                    })
+                    .map((pokemon) => {
+                        return (
+                            <PokemonCard
+                                cardColor={getColors(pokemon.type[0])}
+                                key={pokemon.id}
+                                pokemon={pokemon}
+                            />
+                        );
+                    })}
+            </CardsContainer>
+        </>
+    );
 }
 
 export default App;
